0c804704b1409b432147120208dc06a3cbe057a7
ABIJOSEPHGEORGE <abijosephgeorge@gmail.com>
2023-02-21 19:26:02 +0530

    fixed edit category page load error

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("notfound");
+  res.render("error");
 });
 
 const port = process.env.PORT || 8000;
